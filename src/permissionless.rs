0fa65f5f5a2849850291c21631d37f2eed2a8313
sun <huachuang20@gmail.com>
2020-01-01 14:51:25 +0800

    use default join type

--- src/permissionless.rs
+++ src/permissionless.rs
@@ -1,20 +1,18 @@
-use tdn::{Group, GroupId, PeerId};
+use tdn::{Group, GroupId, PeerAddr};
 
 #[derive(Default)]
 pub struct PermissionlessGroup {
     id: GroupId,
-    peers: Vec<PeerId>,
+    peers: Vec<PeerAddr>,
 }
 
 impl Group for PermissionlessGroup {
-    type JoinType = ();
-
     fn id(&self) -> &GroupId {
         &self.id
     }
 
     /// directly add a peer to group.
-    fn add(&mut self, peer_id: &PeerId) {
+    fn add(&mut self, peer_id: &PeerAddr) {
         self.peers.push(peer_id.clone());
     }
 }
